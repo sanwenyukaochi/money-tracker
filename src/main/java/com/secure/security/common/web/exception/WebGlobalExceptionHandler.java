fe1d4617781149cab65c7f6dd8b5b112e41555e7
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-02 22:46:06 +0800

    optimize: 优化全局异常处理

--- src/main/java/com/secure/security/common/web/exception/WebGlobalExceptionHandler.java
+++ src/main/java/com/secure/security/common/web/exception/WebGlobalExceptionHandler.java
@@ -1,20 +1,12 @@
 package com.secure.security.common.web.exception;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.secure.security.common.web.constant.ResponseCodeConstants;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.HashMap;
-import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.secure.security.domain.model.dto.Result;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -24,49 +16,25 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @RequiredArgsConstructor
 public class WebGlobalExceptionHandler {
 
-    private final ObjectMapper objectMapper;
+    @ExceptionHandler(value = BaseException.class)
+    public Result<?> exceptionHandler(HttpServletResponse response, BaseException e) {
+        response.setStatus(e.getHttpStatus().value());
+        log.warn("业务异常：code={}, msg={}", e.getCode(), e.getMessage());
+        return Result.builder().code(e.getCode()).message(e.getMessage()).build();
+    }
 
     @ExceptionHandler(value = Exception.class)
     public Result<?> exceptionHandler(HttpServletResponse response, Exception e) {
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        log.error("服务器异常", e);
-        return Result.error("服务器异常");
+        log.warn("系统异常：code={}, msg={}", ResponseCodeConstants.SYSTEM_ERROR, e.getMessage());
+        return Result.builder().code(ResponseCodeConstants.SYSTEM_ERROR).message("系统异常").build();
     }
 
     @ExceptionHandler(value = NoResourceFoundException.class)
     public Result<?> exceptionHandler(HttpServletResponse response, NoResourceFoundException e) {
         response.setStatus(HttpStatus.NOT_FOUND.value());
-        return Result.builder()
-                .message("api not found")
-                .code(ResponseCodeConstants.API_NOT_FOUND)
-                .build();
+        log.warn("资源找不到：code={}, msg={}", ResponseCodeConstants.RESOURCE_NOT_FOUND, e.getMessage());
+        return Result.builder().code(ResponseCodeConstants.RESOURCE_NOT_FOUND).message(e.getMessage()).build();
     }
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public Result<?> exceptionHandler(HttpServletResponse response, MethodArgumentNotValidException e) throws JsonProcessingException {
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-
-        // 国际化翻译 数据校验异常信息
-        BindingResult result = e.getBindingResult();
-        List<FieldError> fieldErrors = result.getFieldErrors();
-        HashMap<String, String> errorFields = new HashMap<>();
-        for (FieldError error : fieldErrors) {
-            String fieldName = error.getField();
-            errorFields.put(fieldName, error.getDefaultMessage());
-        }
-        return Result.error(objectMapper.writeValueAsString(errorFields));
-    }
-
-    @ExceptionHandler(value = BaseException.class)
-    public Result<?> exceptionHandler(HttpServletResponse response, BaseException e) {
-        response.setStatus(e.getHttpStatus().value());
-        return createResult(e);
-    }
-
-    private Result<?> createResult(BaseException e) {
-        return Result.builder()
-                .message(e.getMessage())
-                .code(e.getCode() == null ? ResponseCodeConstants.ERROR : e.getCode())
-                .build();
-    }
 }
