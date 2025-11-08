08290e4f56803d4c3043eddd91a60cf14e59d289
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-08 18:11:46 +0800

    style: 调整代码样式

--- src/main/java/com/secure/security/authentication/handler/auth/LoginFailHandler.java
+++ src/main/java/com/secure/security/authentication/handler/auth/LoginFailHandler.java
@@ -2,20 +2,19 @@ package com.secure.security.authentication.handler.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.secure.security.common.web.constant.ResponseCodeConstants;
+import com.secure.security.domain.model.dto.Result;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
 import lombok.RequiredArgsConstructor;
-import com.secure.security.domain.model.dto.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * AbstractAuthenticationProcessingFilter抛出AuthenticationException异常后，会跑到这里来
@@ -30,7 +29,6 @@ public class LoginFailHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException e) throws IOException, ServletException {
-        String errorMessage = e.getMessage();
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
@@ -38,7 +36,7 @@ public class LoginFailHandler implements AuthenticationFailureHandler {
         objectMapper.writeValue(response.getOutputStream(), Result.builder()
                 .data(null)
                 .code(ResponseCodeConstants.LOGIN_FAIL)
-                .message(errorMessage)
+                .message(e.getMessage())
                 .build());
 
     }
