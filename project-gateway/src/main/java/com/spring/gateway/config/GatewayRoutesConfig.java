ffd75b3784d827d8bf5d42b0baf597b783f07f56
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-25 23:29:11 +0800

    feat: 新增Gateway转发配置

---
+++ project-gateway/src/main/java/com/spring/gateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,27 @@
+package com.spring.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("authentication_route", r -> r
+                .path(
+                    "/api/**",
+                    "/api/public-api/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/swagger-resources/**",
+                    "/webjars/**"
+                )
+                .uri("http://localhost:8081"))
+            .build();
+    }
+}
