9de769f2cef8b067e87bdea6ca024414e6cb81d6
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-25 21:38:58 +0800

    feat: 新增测试文件

---
+++ project-authentication/src/test/java/com/authentication/auth/AuthenticationApplicationTests.java
@@ -0,0 +1,10 @@
+package com.authentication.auth;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthenticationApplicationTests {
+
+    @Test
+    void test() {
+    }
+}
---
+++ project-gateway/src/test/java/com/gateway/auth/GatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.gateway.auth;
+
+import org.junit.jupiter.api.Test;
+
+public class GatewayApplicationTests {
+
+    @Test
+    void test() {
+    }
+}
