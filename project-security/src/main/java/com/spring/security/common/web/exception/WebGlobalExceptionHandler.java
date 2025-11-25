3d6dcf4f669d54c61c0ac770f7a5df8c62988632
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-26 00:43:58 +0800

    optimize: 优化异常

--- project-security/src/main/java/com/spring/security/common/web/exception/WebGlobalExceptionHandler.java
+++ project-security/src/main/java/com/spring/security/common/web/exception/WebGlobalExceptionHandler.java
@@ -20,28 +20,28 @@ public class WebGlobalExceptionHandler {
     @ExceptionHandler(value = BaseException.class)
     public Result<?> exceptionHandler(HttpServletResponse response, BaseException e) {
         response.setStatus(e.getHttpStatus().value());
-        log.warn("业务异常：code={}, msg={}", e.getCode(), e.getMessage());
+        log.warn("业务异常：code={}, msg={}", e.getCode(), e.getMessage(), e);
         return Result.error(e.getCode(),e.getMessage());
     }
 
     @ExceptionHandler(value = Exception.class)
     public Result<?> exceptionHandler(HttpServletResponse response, Exception e) {
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        log.warn("系统异常：code={}, msg={}", ResponseCodeConstants.SYSTEM_ERROR, e.getMessage());
+        log.warn("系统异常：code={}, msg={}", ResponseCodeConstants.SYSTEM_ERROR, e.getMessage(), e);
         return Result.error(ResponseCodeConstants.SYSTEM_ERROR,"系统异常");
     }
 
     @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
     public Result<?> exceptionHandler(HttpServletResponse response, HttpRequestMethodNotSupportedException e) {
         response.setStatus(HttpStatus.METHOD_NOT_ALLOWED.value());
-        log.warn("请求方法异常：code={}, msg={}", ResponseCodeConstants.HTTP_METHOD_NOT_ALLOWED, e.getMessage());
+        log.warn("请求方法异常：code={}, msg={}", ResponseCodeConstants.HTTP_METHOD_NOT_ALLOWED, e.getMessage(), e);
         return Result.error(ResponseCodeConstants.HTTP_METHOD_NOT_ALLOWED,"请求方法不支持"+ e.getMethod());
     }
 
     @ExceptionHandler(value = NoResourceFoundException.class)
     public Result<?> exceptionHandler(HttpServletResponse response, NoResourceFoundException e) {
         response.setStatus(HttpStatus.NOT_FOUND.value());
-        log.warn("资源找不到：code={}, msg={}", ResponseCodeConstants.RESOURCE_NOT_FOUND, e.getMessage());
+        log.warn("资源找不到：code={}, msg={}", ResponseCodeConstants.RESOURCE_NOT_FOUND, e.getMessage(), e);
         return Result.error(ResponseCodeConstants.RESOURCE_NOT_FOUND,e.getMessage());
     }
 
