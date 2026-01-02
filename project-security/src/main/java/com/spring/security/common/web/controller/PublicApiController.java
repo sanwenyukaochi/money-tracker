0ce7f9eef09837655b24214757d2c5b74d1f625d
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-01-03 00:32:31 +0800

    optimize: 优化公共接口

--- project-security/src/main/java/com/spring/security/common/web/controller/PublicApiController.java
+++ project-security/src/main/java/com/spring/security/common/web/controller/PublicApiController.java
@@ -1,11 +1,15 @@
 package com.spring.security.common.web.controller;
 
+import com.spring.security.domain.model.dto.Result;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -16,8 +20,10 @@ public class PublicApiController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping
-    public void test() {
+    public Result<Map<String, String>> encode() {
         String encode = passwordEncoder.encode("admin");
-        IO.println(encode);
+        return Result.success("执行成功", Map.of(
+                "passwordEncoder", Optional.ofNullable(encode).orElseThrow(IllegalArgumentException::new)
+        ));
     }
 }
